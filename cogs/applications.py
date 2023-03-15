f3b3e66e954e5e198914ace4e4c26ca9a09e6703
EliteNarNar <77871782+EliteNarNar@users.noreply.github.com>
2023-03-15 19:53:28 +0000

    final changes

--- cogs/applications.py
+++ cogs/applications.py
@@ -73,7 +73,10 @@ class Applications(interactions.Extension):
         scraper = cloudscraper.create_scraper()
         res = scraper.get("https://api.tracker.gg/api/v2/rocket-league/standard/profile/epic/" + response)
         print(res.text)
-        data = json.loads(res.text)
+        try:
+            data = json.loads(res.text)
+        except:
+            await ctx.send("There was a problem accessing the tracker api, please try again later. Contact admins if this keeps happening.", ephemeral=True)
         if "errors" in data:
             await ctx.send("There was a problem finding the data for this account (it probably doesn't exist), if you feel this was in error contact Sneakynarnar#7573")
             
@@ -202,7 +205,7 @@ class Applications(interactions.Extension):
                 try:
                     data = json.loads(res.text)
                 except:
-                    print("There was a problem accessing the tracker api, please try again later. Contact admins if this keeps happening.")
+                    await ctx.send("There was a problem accessing the tracker api, please try again later. Contact admins if this keeps happening.", ephemeral=True)
                 if "errors" in data:
                     await ctx.send("There was a problem finding the data for this account (it probably doesn't exist), if you feel this was in error contact Sneakynarnar#7573", ephemeral=True)
                     
