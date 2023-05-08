b36d5031dea493b573aceb4be922866be05fe042
EliteNarNar <77871782+EliteNarNar@users.noreply.github.com>
2023-05-08 19:58:42 +0100

    fixing submit command

--- cogs/cotw.py
+++ cogs/cotw.py
@@ -109,7 +109,8 @@ class COTW(interactions.Extension):
         
         channel = interactions.Channel(**await self.bot._http.get_channel(self.channel), _client=self.bot._http)
         cur = connect(host)
-        messages = channel.history(start_at=ctx.target.id)
+        history = channel.history(start_at=ctx.target.id)
+        messages = await channel.flatten()
         status = ""
         for message in messages:
             asyncio.sleep(0.1)
